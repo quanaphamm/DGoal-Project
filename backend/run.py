85553d62bc57ab410952bd14f3fc016babf7362d
quanaphamm <mr.impossible4068@gmail.com>
2025-03-09 17:21:53 -0700

    fix flask

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,7 @@
-import os
 from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port, debug=False)
+
+if __name__ != "__main__":
+    gunicorn_app = app  
