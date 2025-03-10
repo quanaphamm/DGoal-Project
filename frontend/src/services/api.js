ad14d770a7f636c98bbdb40a59c7d75edd3fa8d9
quanaphamm <mr.impossible4068@gmail.com>
2025-03-09 17:45:45 -0700

    fix url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // ✅ Backend API Base URL
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://dgoal-project.onrender.com";
+
 
 /**
  * ✅ Login User
